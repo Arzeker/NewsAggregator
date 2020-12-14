6da9d91a207609af0c32ddc4213a7614e2761ae6
olunds <olunds@kth.se>
2020-12-14 15:28:13 +0100

    update

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('main');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
