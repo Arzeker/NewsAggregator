3abcdef216c6506caa6b8cc5dfceb4780adc955d
olunds <olunds@kth.se>
2020-12-14 14:17:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const express = require('express')
+const PORT = process.env.PORT || 5000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.render(main);
-});
-
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+express()
+  .get('/', (req, res) => res.render('main'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
